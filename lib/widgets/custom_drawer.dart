0205de77d94f591784e785dcfa5af52969d59953
Kerolos Farah <k.farah2124@gmail.com>
2024-01-17 11:30:13 +0200

    create drawer items

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../models/drawer_item_model.dart';
 import '../utils/app_images.dart';
+import 'active_and_inactive_drawer_item.dart';
+import 'drawer_items_list_view.dart';
 import 'user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -10,12 +13,53 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          UserInfoListTile(
-            image: Assets.imagesAvatar3,
-            title: 'Lekan Okeowo',
-            subtitle: 'demo@gmail.com',
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+              image: Assets.imagesAvatar3,
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com',
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
+          ),
+          DrawerItemsListView(),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 20,
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(
+                  child: SizedBox(),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                    image: Assets.imagesSetting,
+                    title: 'Setting system',
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                    image: Assets.imagesLogout,
+                    title: 'Logout account',
+                  ),
+                ),
+                SizedBox(
+                  height: 48,
+                ),
+              ],
+            ),
           ),
         ],
       ),
