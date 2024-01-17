0b1d3f34baec682e9a571faf0f0a0d9dada66b7d
Kerolos Farah <k.farah2124@gmail.com>
2024-01-17 13:17:13 +0200

    create all expenses header

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'all_expenses.dart';
 import 'custom_drawer.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -11,6 +12,19 @@ class DashboardDesktopLayout extends StatelessWidget {
       children: [
         Expanded(
           child: CustomDrawer(),
+        ),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: [
+              Expanded(
+                child: AllExpenses(),
+              ),
+            ],
+          ),
         ),
       ],
     );
