47f9c74abd933f8afe59624da5fd53a43df539cf
Kerolos Farah <k.farah2124@gmail.com>
2024-01-21 10:34:00 +0200

    create my cards section

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'all_expenses_and_quick_invoice_section.dart';
 import 'custom_drawer.dart';
+import 'my_cards_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -17,8 +18,15 @@ class DashboardDesktopLayout extends StatelessWidget {
           width: 32,
         ),
         Expanded(
-          flex: 2,
+          flex: 3,
           child: AllExpensesAndQuickInVoiceSection(),
+        ),
+        SizedBox(
+          width: 24,
+        ),
+        Expanded(
+          flex: 2,
+          child: MyCardsSection(),
         ),
       ],
     );
